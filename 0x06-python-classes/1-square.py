777239f0b3becfd4f329edb57aceb1c62c1adbc8
lulokent <cwengalulonkem@gmail.com>
2024-01-24 19:13:37 -0800

    file update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #/usr/bin/python3
+"""A module for square"""
 class Square:
+    """Represent a square"""
     def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Define a class of square"""
 class Square:
+    """Represent a square"""
     def __init__(self, size=0):
+        """Initializing a new Square.
+        Args:
+            size(int): The size of the new square.
+        Raise:
+            TypeError: If the size is not an integer
+            ValueError: If the the size is less than 0.
+        """
        if not isinstance(size, int):
            raise TypeError("size must be an integer")
        elif size < 0:
