ed40c9bb5a8bebebe9478bf91e411a2497088dee
lulokent <cwengalulonkem@gmail.com>
2024-04-28 01:33:49 -0700

    task 4 and 5 updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A script that takes in a URL,sends a POST request to the passed URL
 # displays the body of response
-curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
