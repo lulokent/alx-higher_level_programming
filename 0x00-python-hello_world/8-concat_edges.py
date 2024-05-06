33864034d8459ee44920ef91588256a271673f47
lulokent <cwengalulonkem@gmail.com>
2024-05-06 00:22:55 -0700

    updated files

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -3,6 +3,5 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
 
-str = str[39:66] + ' ' + str[-22:-18] + ' ' + str[:6]
-
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
