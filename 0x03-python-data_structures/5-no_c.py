f6e0173a0dab04c0d6c5dbc5a426356b07bbe2e2
lulonkent <cwengalulonkem@gmail.com>
2024-07-10 11:45:59 +0200

    removes letters in a string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """funtion that rwemoves all characters c and C."""
+    str_copy = ""
+    for element in my_string:
+        if element != 'c' and element != 'C':
+            str_copy += element
+    return str_copy
