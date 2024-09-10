a4bd2f5bdeafeea73160a653673908543c2afb6c
Cwenga  Mkentane <144338571+lulokent@users.noreply.github.com>
2024-09-10 04:13:10 +0200

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/v2-create_read_user.sql
+++
@@ -1,7 +0,0 @@
---creates a database hbtn_)d_2 and the user user_0d_2
---creates a database
-CREATE DATABASE IF NOT EXIST htbn_0d_2;
---Create a user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd'
---Grants select privileges to a user
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
