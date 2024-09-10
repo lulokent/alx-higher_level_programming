b9471679b95ccb8e8dd61f47a02f7a258fe1d3f6
Cwenga  Mkentane <144338571+lulokent@users.noreply.github.com>
2024-09-10 04:14:05 +0200

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def magic_string(static={"count": 0}):
-    """Returns a string 'BestSchool' n times"""
-    static["count"] += 1
-    return str("BestSchool, " *static["count"])[:-2]
--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""LockedClass module."""
-
-class LockedClass:
-    """LockedClass containing only __slots__."""
-    __slots__ = ['first_name']
