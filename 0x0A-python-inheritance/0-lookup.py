3d8c8007b825b687e8edda2d5a4b4d767913b199
Cwenga  Mkentane <144338571+lulokent@users.noreply.github.com>
2024-09-10 04:13:46 +0200

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def lookup(obj):
-    """Returns a list object's avaivable attributes.
-
-    Args:
-        obj: The input object to be processed.
-    """
-    return (dir(obj))
