a40f95c117129a1b7c1b3b898139db7b4c654cb7
root <cwengalulonkem@gmail.com>
2024-09-10 23:34:08 -0700

    file 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
 print(str[39:56] + str[56:66] + " " + "with" + " " + str[:6])
