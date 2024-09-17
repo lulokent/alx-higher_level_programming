773efd36829d605e692845d838699d012287e829
root <cwengalulonkem@gmail.com>
2024-09-16 20:10:33 -0700

    tasks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,13 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
+
 def new_in_list(my_list, idx, element):
-    """A function that replace an element at a specific position."""
+    """A function that replaces an element at a specific position."""
+
     if idx < 0:
         return my_list
     elif idx >= len(my_list):
         return my_list
+
     temp = list(my_list)
     temp[idx] = element
     return temp
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 def max_integer(my_list=[]):
     """Function that finds the biggest integerof a list."""
+
     if my_list:
-       my_list.sort(reverse=True)
-       return (my_list[0])
+        my_list.sort(reverse=True)
+        return (my_list[0])
     return (None)
